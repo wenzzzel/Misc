7d83beadb1e3a5cce039a1757630154617602ee2
Erik Wenzel <EWENTZEL@volvocars.com>
2022-01-25 18:30:25 +0100

    Added powershell directory and updated .gitignore

---
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,6 @@
+$ENV:PATH += ";C:\Program Files\Git\bin";
+$ENV:PATH += ";C:\Program Files\nodejs\";
+$ENV:PATH += ";C:\Users\ewentzel\AppData\Roaming\npm";
+$ENV:PATH += ";C:\Program Files\Neovim\bin";
+
+cd "C:\Users\ewentzel\source\repos";
