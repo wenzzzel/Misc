85eb8279bb18c91e520e324606d6f4adcf571f5d
wenzzzel <wenzzzel@live.se>
2022-01-26 19:28:14 +0100

    Added write-host & user defined functions section

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,7 +1,15 @@
+Write-Host "Adding to path variable" -ForegroundColor Green;
 $ENV:PATH += ";C:\Program Files\Git\bin";
 $ENV:PATH += ";C:\Program Files\nodejs\";
 $ENV:PATH += ";C:\Users\ewentzel\AppData\Roaming\npm";
 $ENV:PATH += ";C:\Program Files\Neovim\bin";
-#
 
+Write-Host "Stepping into C:\Users\ewentzel\source\repos\" -ForegroundColor Green;
 cd "C:\Users\ewentzel\source\repos";
+
+Write-Host "Creating user defined functions" -ForegroundColor Green;
+Write-Host "    Open-NvimNotes" -ForegroundColor Blue;
+function Open-NvimNotes {
+    Write-Host "Opening notes with nvim" -ForegroundColor Green;
+    nvim "C:\Users\ewentzel\Crap\Notes\note1.txt";
+}
