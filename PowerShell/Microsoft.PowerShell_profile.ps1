a1ae61dc4c27e270ced3a52f575fb46c794db86a
wenzzzel <wenzzzel@live.se>
2022-01-26 19:44:07 +0100

    Changed colors in $PROFILE

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,15 +1,15 @@
-Write-Host "Adding to path variable" -ForegroundColor Green;
+Write-Host "Adding to path variable" -ForegroundColor Blue;
 $ENV:PATH += ";C:\Program Files\Git\bin";
 $ENV:PATH += ";C:\Program Files\nodejs\";
 $ENV:PATH += ";C:\Users\ewentzel\AppData\Roaming\npm";
 $ENV:PATH += ";C:\Program Files\Neovim\bin";
 
-Write-Host "Stepping into C:\Users\ewentzel\source\repos\" -ForegroundColor Green;
+Write-Host "Stepping into C:\Users\ewentzel\source\repos\" -ForegroundColor Blue;
 cd "C:\Users\ewentzel\source\repos";
 
-Write-Host "Creating user defined functions" -ForegroundColor Green;
-Write-Host "    Open-NvimNotes" -ForegroundColor Blue;
+Write-Host "Creating user defined functions" -ForegroundColor Blue;
+Write-Host "    Open-NvimNotes" -ForegroundColor Yellow;
 function Open-NvimNotes {
-    Write-Host "Opening notes with nvim" -ForegroundColor Green;
+    Write-Host "Opening notes with nvim" -ForegroundColor Blue;
     nvim "C:\Users\ewentzel\Crap\Notes\note1.txt";
 }
