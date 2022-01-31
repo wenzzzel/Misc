b7382a31ffac5c1c6bc219b23144955d039f3876
wenzzzel <wenzzzel@live.se>
2022-01-31 11:58:26 +0100

    Added azure data migration tool to path

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -3,6 +3,7 @@ $ENV:PATH += ";C:\Program Files\Git\bin";
 $ENV:PATH += ";C:\Program Files\nodejs\";
 $ENV:PATH += ";C:\Users\ewentzel\AppData\Roaming\npm";
 $ENV:PATH += ";C:\Program Files\Neovim\bin";
+$ENV:PATH += ";C:\Program Files\azure-documentdb-datamigrationtool-1.8.3";
 
 Write-Host "Stepping into C:\Users\ewentzel\source\repos\" -ForegroundColor Blue;
 cd "C:\Users\ewentzel\source\repos";
