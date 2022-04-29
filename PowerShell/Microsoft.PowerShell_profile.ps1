00d38456b59a0e21b0ca7cf6e85e00da25b14be8
wenzzzel <wenzzzel@live.se>
2022-04-29 14:56:23 +0200

    Added IncludeIntegrationTests parameter to Test-Unit

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -47,16 +47,25 @@ function Get-AzureKeyVaults {
 Write-Host "    Test-Unit" -ForegroundColor Yellow;
 function Test-Unit {
     param (
-        [Parameter(Mandatory=$false)][string]$TestName
+        [Parameter(Mandatory=$false)][string]$TestName,
+        [Parameter(Mandatory=$false)][switch]$IncludeIntegrationTests
     )
     
     if($PSBoundParameters.ContainsKey('TestName')){
         Write-Host "dotnet test --filter name=$TestName" -ForegroundColor Blue;
         dotnet test --filter "name=$TestName"
-    }else{
+        return;
+    }
+
+    if($IncludeIntegrationTests.IsPresent){
         Write-Host "dotnet test" -ForegroundColor Blue;
         dotnet test
+        return
     }
+    
+    Write-Host "dotnet test --filter TestCategory!=Integration" -ForegroundColor Blue;
+    dotnet test --filter TestCategory!=Integration
+    return;
 }
 New-Alias -Name test -Value Test-Unit;
 
