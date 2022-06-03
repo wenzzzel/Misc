b5ddb3dcc2a422f36ed8b4033daa9826fea56078
wenzzzel <wenzzzel@live.se>
2022-06-03 15:48:11 +0200

    Added summary to the output of Find-NuGet

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -78,7 +78,7 @@ function Find-NuGet {
     Write-Host "Find-Package -Source nuget.org -Name *$NugetName*" -ForegroundColor Blue;
     $Packages = Find-Package -Source nuget.org -Name *$NugetName*
 
-    $FormattedList = $Packages | Select-Object Name, @{ Name = 'LatestVersion'; Expression = { $_.Version }};
+    $FormattedList = $Packages | Select-Object Name, @{ Name = 'LatestVersion'; Expression = { $_.Version }}, Summary;
     return $FormattedList
 }
 New-Alias -Name fng -Value Find-NuGet;
