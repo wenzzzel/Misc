c09fbef52dbe832efed343b2da588b989fca62d6
wenzzzel <wenzzzel@live.se>
2022-06-21 15:44:27 +0200

    Created user defined function New-LocalSettingsJson

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -231,6 +231,49 @@ function New-DotnetVsCodeLaunch {
 
     return
 }
+Write-Host "    New-LocalSettingsJson" -ForegroundColor Yellow;
+function New-LocalSettingsJson {
+    #TODO: Clean up alot
+    #TODO: Figure out Profile name instead of setting placeholder <projectRootFolderName>
+    #TODO: Maybe add an alias to this function?
+    param (
+        [Parameter(Mandatory=$false)][string]$Csproj
+    )
+
+    $projectDir = "";
+    if($PSBoundParameters.ContainsKey('Csproj')){
+        Write-Host "Csproj was provided. Working in it's folder."
+        $projectDir = Get-Item $Csproj | select -ExpandProperty Directory | select -ExpandProperty FullName
+    }else{
+        Write-Host "No csproj provided. Trying to find it manually."
+        $projectDir = ls -Recurse | ? -Property Extension -eq '.csproj' | ? -Property Name -NotLike '*test*' | select -ExpandProperty Directory | select -ExpandProperty FullName;
+        
+        if(!(Test-Path $projectDir) || $projectDir.count -ne 1){
+            throw "Something happened when getting the projectDir";
+        }
+    }
+
+    #TODO: A check for azure function project, so we cant add this in, for example, a web-api
+
+    $localsettingsFile = "$projectDir/local.settings.json";
+    if(Test-Path $localsettingsFile){
+        Write-Host "Launchsettings already exists. Why have you run this command? Do you think I'm a joke you little shit!?"
+    }else{
+        $localsettingsFileContent = '
+{
+    "IsEncrypted": false,
+    "Values": {
+        "FUNCTIONS_WORKER_RUNTIME": "dotnet",
+        "AzureWebJobsStorage": "UseDevelopmentStorage=true"
+    }
+}
+        '
+        Write-Host "Creating launchsettings.json";
+        ni -ItemType File -Path $localsettingsFile -Value $localsettingsFileContent;
+    }
+
+    return
+}
 
 #TODO: Create function for generating the functions setting (json list of functions to run) for azure functions project
 
