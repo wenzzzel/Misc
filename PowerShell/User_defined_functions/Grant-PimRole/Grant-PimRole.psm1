c935962c41202d4a68ef4a95f414a21a04cdb7f5
wenzzzel <wenzzzel@live.se>
2023-08-25 09:25:51 +0200

    Fix: Commented out part that's not needed

--- PowerShell/User_defined_functions/Grant-PimRole/Grant-PimRole.psm1
+++ PowerShell/User_defined_functions/Grant-PimRole/Grant-PimRole.psm1
@@ -26,22 +26,25 @@ function Grant-PimRole {
         -ScheduleInfoStartDateTime $startTime `
         -Justification $Justification
 
-    ###############################
-    #Add for Test/QA subscription #
-    ###############################
-    $RoleDefinitionID = "b24988ac-6180-42a0-ab88-20f7382dd24c" # Contributor
-    $scope = "/subscriptions/43d4530a-736d-4515-b5c5-e43adbe8c02c" # Non-prod subscription
-    $guid = (New-Guid)
-    $startTime = Get-Date -Format o
+    # Comment out below part, since it seems that this subscription is not needed anymore.
+    # Don't dare to fully delete it quite yet though...
 
-    New-AzRoleAssignmentScheduleRequest `
-    -Name $guid `
-    -Scope $scope `
-    -ExpirationDuration PT2H `
-    -ExpirationType AfterDuration `
-    -PrincipalId $userObjectID `
-    -RequestType SelfActivate `
-    -RoleDefinitionId /providersproviders/Microsoft.Management/managementGroups/$managementgroupID/providers/Microsoft.Authorization/roleDefinitions/$roledefinitionId `
-    -ScheduleInfoStartDateTime $startTime `
-    -Justification $Justification
+    # ###############################
+    # #Add for Test/QA subscription #
+    # ###############################
+    # $RoleDefinitionID = "b24988ac-6180-42a0-ab88-20f7382dd24c" # Contributor
+    # $scope = "/subscriptions/43d4530a-736d-4515-b5c5-e43adbe8c02c" # Non-prod subscription
+    # $guid = (New-Guid)
+    # $startTime = Get-Date -Format o
+
+    # New-AzRoleAssignmentScheduleRequest `
+    # -Name $guid `
+    # -Scope $scope `
+    # -ExpirationDuration PT2H `
+    # -ExpirationType AfterDuration `
+    # -PrincipalId $userObjectID `
+    # -RequestType SelfActivate `
+    # -RoleDefinitionId /providersproviders/Microsoft.Management/managementGroups/$managementgroupID/providers/Microsoft.Authorization/roleDefinitions/$roledefinitionId `
+    # -ScheduleInfoStartDateTime $startTime `
+    # -Justification $Justification
 }
