5135174be45cdfc6576cae0aac8b597ad6d3c17f
wenzzzel <wenzzzel@live.se>
2023-10-05 19:54:24 +0200

    Added posh-git and oh-my-posh

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -59,6 +59,8 @@ $chocoPackages = @{
     "powertoys" = "C:\Program Files\PowerToys\"
     "slack" = "C:\Program Files\Slack\"
     "daxstudio" = "C:\Program Files\Dax Studio\"
+    "poshgit" = "C:\tools\poshgit\dahlbyk-posh-git-9bda399\"
+    "oh-my-posh" = "C:\Program Files (x86)\oh-my-posh\bin\"
 }
 $whiteSpaceCount = 40;
 foreach($package in $chocoPackages.GetEnumerator()){
@@ -220,3 +222,20 @@ if($currentDate.Day -gt 20){
     }
 }
 
+# Activate Intellisense for powershell
+Set-PSReadLineOption -PredictionViewStyle ListView
+Set-PSReadLineOption -EditMode Windows
+
+#Activate posh-git and oh-my-posh
+Write-Host "Trying to start posh-git and oh-my-posh" -ForegroundColor Blue;
+if((Test-Path "C:\tools\poshgit\dahlbyk-posh-git-9bda399\") -And (Test-Path "C:\Program Files (x86)\oh-my-posh\bin\")){
+    Write-Host " ✔️ posh-git and oh-my-posh was found, starting..."
+    Install-Module posh-git
+    Import-Module posh-git -Force
+    
+    Import-Module 'C:\tools\poshgit\dahlbyk-posh-git-9bda399\src\posh-git.psd1'
+    # oh-my-posh font install agave
+    oh-my-posh init pwsh | Invoke-Expression
+} else {
+    Write-Host " ❌ Couldn't start posh-git and oh-my-posh because at least one of them was not found." -ForegroundColor Blue;
+}
