995215614b7b2b3ecd95a54183b37abb88007961
wenzzzel <wenzzzel@live.se>
2023-10-07 13:19:39 +0200

    Added fallback to local note file

--- PowerShell/User_defined_functions/Open-NvimNotes/Open-NvimNotes.psm1
+++ PowerShell/User_defined_functions/Open-NvimNotes/Open-NvimNotes.psm1
@@ -1,4 +1,22 @@
 function Open-NvimNotes {
     Write-Host "Opening notes with nvim" -ForegroundColor Blue;
-    nvim "$env:OneDriveCommercial\Documents\note1.txt" +$;
+
+    if (Test-Path "$env:OneDriveCommercial\Documents\note1.txt"){
+        nvim "$env:OneDriveCommercial\Documents\note1.txt" +$;
+    } else {
+        Write-Host "Couldn't find notes file. Trying to create it.";
+
+        if (Test-Path "$env:OneDriveCommercial\Documents\"){
+            New-Item -ItemType File -Path "$env:OneDriveCommercial\Documents\note1.txt";
+            nvim "$env:OneDriveCommercial\Documents\note1.txt" +$;
+        } else {
+            Write-Host "Couldn't find OneDrive directory to put the file in. Looking for local notes file @ $env:userprofile\Documents\note1.txt instead.";
+
+            if (!(Test-Path "$env:userprofile\Documents\note1.txt")){
+                Write-Host "Couldn't find local notes file. Trying to create it.";
+                New-Item -ItemType File -Path "$env:userprofile\Documents\note1.txt";
+            }
+            nvim "$env:userprofile\Documents\note1.txt" +$;
+        }
+    }
 }
