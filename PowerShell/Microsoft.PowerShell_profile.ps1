42e0f49aab0e484c1143d8998bc20aea3900a43b
wenzzzel <wenzzzel@live.se>
2023-11-05 15:47:34 +0100

    Only checking npm packs and ps modules once daily

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -12,14 +12,14 @@ _________|                                       _    |________
 
 Write-Host $asciiArt -ForegroundColor Green;
 
+& "$PROFILE/../Profile_Scripts/add_user_defined_variables.ps1";
 & "$PROFILE/../Profile_Scripts/add_to_path.ps1";
-& "$PROFILE/../Profile_Scripts/add_choco_packs.ps1"; #Don't check every time?
-& "$PROFILE/../Profile_Scripts/add_npm_packs.ps1"; #Don't check every time?
-& "$PROFILE/../Profile_Scripts/check_ps_modules.ps1"; #Don't check every time?
+& "$PROFILE/../Profile_Scripts/add_choco_packs.ps1"; 
+& "$PROFILE/../Profile_Scripts/add_npm_packs.ps1"; 
+& "$PROFILE/../Profile_Scripts/check_ps_modules.ps1"; 
 & "$PROFILE/../Profile_Scripts/load_ps_secrets.ps1";
 & "$PROFILE/../Profile_Scripts/add_aliases.ps1";
 & "$PROFILE/../Profile_Scripts/setup_choco_tab_completion.ps1";
-& "$PROFILE/../Profile_Scripts/add_user_defined_variables.ps1";
 & "$PROFILE/../Profile_Scripts/add_user_defined_functions.ps1";
 & "$PROFILE/../Profile_Scripts/check_reminders.ps1";
 & "$PROFILE/../Profile_Scripts/setup_choco_tab_completion.ps1";
--- PowerShell/Profile_scripts/check_ps_modules.ps1
+++ PowerShell/Profile_scripts/check_ps_modules.ps1
@@ -1,12 +1,35 @@
 Write-Host "Making sure powershell module dependencies are installed" -ForegroundColor Blue;
+
+$statePath = "$thisRepoRootDir/state.json"
+
+if(!(Test-Path $statePath)){
+    Write-Host "No state.json found @ $statePath . Creating it..." -ForegroundColor Blue;
+    New-Item -ItemType File -Path $statePath -Value "{}"; 
+}
+
+$state = (Get-Content -Raw $statePath | ConvertFrom-Json);
+
+$currentDate = (Get-Date);
+
 $moduleDependencies = @(
     'NuGet',
     'Az',
     'CosmosDB'
 );
 $moduleDependencies = $moduleDependencies.GetEnumerator() | Sort-Object;
+
+if($state.LastPsModuleCheck -eq ($currentDate.Year.ToString() + "-" + $currentDate.Month.ToString() + "-" + $currentDate.Day.ToString())){ 
+    Write-Host " ⏩ Powershell module dependencies were already checked today, skipping this step until tomorrow...";
+    foreach($moduleDependency in $moduleDependencies){
+        Write-Host " ➖ $moduleDependency";
+    }
+
+    return;
+}
+
 $installedModules = Get-InstalledModule | Where-Object -Property Name -In $moduleDependencies;
 
+$allPackagesInstalled = $true;
 foreach($moduleDependency in $moduleDependencies){
     [bool]$IsInstalled = ($installedModules | Where-Object -Property Name -eq $moduleDependency | Measure-Object | Select-Object -ExpandProperty Count)
     if(!$IsInstalled){
@@ -15,6 +38,7 @@ foreach($moduleDependency in $moduleDependencies){
         Write-Host "Checking that $ModuleDependency was successfully installed"
         [bool]$IsInstalled = (Get-InstalledModule | Where-Object -Property Name -eq $moduleDependency | Measure-Object | Select-Object -ExpandProperty Count)
         if(!$IsInstalled){
+            $allPackagesInstalled = $false;
             $errMess = "The dependency $moduleDependency could not be found! Please resolve before running this powershell profile!";
             Write-Host $errMess -ForegroundColor Red;
             Throw $errMess;
@@ -24,4 +48,9 @@ foreach($moduleDependency in $moduleDependencies){
     } else {
         Write-Host " ✔️ $moduleDependency";
     }
+}
+
+if($allPackagesInstalled){
+    Add-Member -InputObject $state -Name LastPsModuleCheck -Value ($currentDate.Year.ToString() + "-" + $currentDate.Month.ToString() + "-" + $currentDate.Day.ToString()) -MemberType NoteProperty;
+    Set-Content -Value ($state | ConvertTo-Json) -Path $statePath;
 }
