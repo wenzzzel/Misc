93d3ce197a7a7d5ec5a4ea1443688be373749149
wenzzzel <wenzzzel@live.se>
2023-11-14 14:02:58 +0100

    Using marcduiker theme for oh-my-posh

--- PowerShell/Profile_scripts/activate_posh.ps1
+++ PowerShell/Profile_scripts/activate_posh.ps1
@@ -3,8 +3,8 @@ Write-Host "Trying to start posh-git and oh-my-posh" -ForegroundColor Blue;
 if((Test-Path "C:\tools\poshgit\dahlbyk-posh-git-9bda399\") -And (Test-Path "C:\Program Files (x86)\oh-my-posh\bin\")){
     Write-Host " ✔️ posh-git and oh-my-posh was found, starting..."   
     Import-Module 'C:\tools\poshgit\dahlbyk-posh-git-9bda399\src\posh-git.psd1'
-    # oh-my-posh font install agave
-    oh-my-posh init pwsh | Invoke-Expression
+    #oh-my-posh font install agave #Nerd font (only need to run first time)
+    oh-my-posh init pwsh --config 'https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/marcduiker.omp.json' | Invoke-Expression # --config parameter is for oh-my-posh-theme. Skip it to get default theme
 } else {
     Write-Host " ❌ Couldn't start posh-git and oh-my-posh because at least one of them was not found." -ForegroundColor Blue;
 }
